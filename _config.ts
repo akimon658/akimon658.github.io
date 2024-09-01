8cba957d953c256b20e14e85639cb2df0dec398e
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-01 18:08:37 +0900

    Enable styling with Emotion

--- _config.ts
+++ _config.ts
@@ -1,5 +1,5 @@
 import lume from "lume/mod.ts"
-import jsx from "lume/plugins/jsx_preact.ts"
+import jsx from "lume/plugins/jsx.ts"
 import multilanguage from "lume/plugins/multilanguage.ts"
 import redirects from "lume/plugins/redirects.ts"
 
--- content/_includes/layouts/blog.tsx
+++ content/_includes/layouts/blog.tsx
@@ -1,8 +1,15 @@
+import styled from "@emotion/styled"
+
 export const layout = "layouts/root.tsx"
 
+const Article = styled.article({
+  margin: "0 auto",
+  maxWidth: "50rem",
+})
+
 export default ({ title, children }: Lume.Data, _helpers: Lume.Helpers) => (
-  <article>
+  <Article>
     <h1>{title}</h1>
     {children}
-  </article>
+  </Article>
 )
