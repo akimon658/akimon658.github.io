792e440e7b82390348f41bafec877fb3de59785f
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-30 16:53:19 +0900

    Create header

--- src/_includes/layouts/root.tsx
+++ src/_includes/layouts/root.tsx
@@ -5,6 +5,9 @@ export default ({ title, children }: Lume.Data, _helpers: Lume.Helpers) => (
       <link rel="stylesheet" href="/style.css" />
     </head>
     <body>
+      <header className="flex h-14 items-center mx-auto max-w-5xl">
+        <a href="/" className="ml-4 my-auto text-xl">akimon658.github.io</a>
+      </header>
       <main>{children}</main>
     </body>
   </html>
