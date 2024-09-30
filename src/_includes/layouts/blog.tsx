b29ab1c03298d2dcdcbbbb4dbefe9771de562a3a
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-30 15:46:43 +0900

    Change headings font weight

--- src/_includes/layouts/blog.tsx
+++ src/_includes/layouts/blog.tsx
@@ -1,7 +1,7 @@
 export const layout = "layouts/root.tsx"
 
 export default ({ title, children }: Lume.Data, _helpers: Lume.Helpers) => (
-  <article className="prose mx-auto">
+  <article className="mx-auto prose prose-headings:font-normal">
     <h1>{title}</h1>
     {children}
   </article>
