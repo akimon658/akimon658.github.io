0211a23d78fec2c70c13ee45e781049162402df0
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-09-30 12:04:08 +0900

    Set `mx-auto` for blog content

--- src/_includes/layouts/blog.tsx
+++ src/_includes/layouts/blog.tsx
@@ -1,7 +1,7 @@
 export const layout = "layouts/root.tsx"
 
 export default ({ title, children }: Lume.Data, _helpers: Lume.Helpers) => (
-  <article className="prose">
+  <article className="prose mx-auto">
     <h1>{title}</h1>
     {children}
   </article>
