fcdd0b24e8a8d24b1ae934395cd84d88f227bee4
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-10-02 10:07:13 +0900

    Use _data.yml to change content by language (WIP)

---
+++ src/_components/toppage.tsx
@@ -0,0 +1,78 @@
+export default (data: Lume.Data, _helpers: Lume.Helpers) => {
+  const comp = data.comp
+
+  if (data.lang === "en") {
+    data = data.en
+  }
+
+  return (
+    <>
+      <div className="
+        flex
+        items-center
+        justify-center
+        mt-8
+      ">
+        <img src="/img/icon.png" />
+        <div className="
+          ml-14
+          text-xl
+        ">
+          <h1 className="
+            font-bold
+            text-2xl
+          ">
+            {data.author_name}
+          </h1>
+          <div className="text-gray-500">
+            @akimon658
+          </div>
+          <div className="mt-2">
+            {data.author_affiliation}
+          </div>
+          <div className="
+            flex
+            mt-4
+          ">
+            <comp.Link
+              href="https://x.com/akimon658"
+              title="X"
+              className="mr-4"
+            >
+              <img src="/icon/x_black.png" className="h-6" />
+            </comp.Link>
+            <comp.Link
+              href="https://github.com/akimon658"
+              title="GitHub"
+              className="mr-4"
+            >
+              <img src="/icon/github_black.svg" className="h-6" />
+            </comp.Link>
+            <comp.Link
+              href="https://atcoder.jp/users/akimon658"
+              title="AtCoder"
+              className="" // To avoid open_in_new icon to be displayed
+            >
+              <img src="/icon/atcoder_black.png" className="h-6" />
+            </comp.Link>
+          </div>
+        </div>
+      </div>
+      <h2 className="
+        mt-8
+        text-2xl
+        text-center
+      ">
+        {data.article}
+      </h2>
+      <comp.List
+        query="
+          category=blog
+          lang=ja
+          layout=layouts/blog.tsx
+        "
+        sort="date=desc"
+      />
+    </>
+  )
+}
--- src/index.en.tsx
+++ src/index.en.tsx
@@ -1,18 +1,6 @@
 export const title = "akimon658.github.io"
+export const lang = "en"
 export const layout = "layouts/root.tsx"
 export const url = "/en/"
 
-export default (data: Lume.Data, _helpers: Lume.Helpers) => (
-  <>
-    <h1>{data.title}</h1>
-    <p>Under construction</p>
-    <ul>
-      <li>
-        <a href="/blog/">Blog</a>
-      </li>
-      <li>
-        <a href="/privacy-policy/">Privacy Policy</a>
-      </li>
-    </ul>
-  </>
-)
+export default ({ comp }: Lume.Data, _helpers: Lume.Helpers) => <comp.TopPage />
