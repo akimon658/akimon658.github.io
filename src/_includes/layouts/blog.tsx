aa840ef04895421a9649f7261799d9411ea32dd6
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-10-05 14:53:53 +0900

    Change date format by page language (close #81)

--- src/_includes/layouts/blog.tsx
+++ src/_includes/layouts/blog.tsx
@@ -1,32 +1,38 @@
 export const layout = "layouts/root.tsx"
 
-export default (
-  { date, title, children }: Lume.Data,
-  _helpers: Lume.Helpers,
-) => (
-  <article className="
-    hover:prose-a:underline
-    mx-auto
-    prose
-    prose-a:font-normal
-    prose-a:no-underline
-    prose-a:text-blue-600
-    prose-code:after:content-none
-    prose-code:before:content-none
-    prose-code:font-normal
-    prose-headings:font-normal
-    px-4
-    visited:prose-a:text-purple-700
-  ">
-    <h1>{title}</h1>
-    <time
-      dateTime={`${date.getFullYear()}-${
-        date.getMonth() + 1
-      }-${date.getDate()}`}
-      className="text-gray-500"
-    >
-      {`${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`}
-    </time>
-    {children}
-  </article>
-)
+export default (data: Lume.Data, _helpers: Lume.Helpers) => {
+  const date = data.date
+  const locale = data.lang === "ja" ? "ja-JP" : "en-US"
+
+  return (
+    <article className="
+      hover:prose-a:underline
+      mx-auto
+      prose
+      prose-a:font-normal
+      prose-a:no-underline
+      prose-a:text-blue-600
+      prose-code:after:content-none
+      prose-code:before:content-none
+      prose-code:font-normal
+      prose-headings:font-normal
+      px-4
+      visited:prose-a:text-purple-700
+    ">
+      <h1>{data.title}</h1>
+      <time
+        dateTime={`${date.getFullYear()}-${
+          date.getMonth() + 1
+        }-${date.getDate()}`}
+        className="text-gray-500"
+      >
+        {date.toLocaleDateString(locale, {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </time>
+      {data.children}
+    </article>
+  )
+}
