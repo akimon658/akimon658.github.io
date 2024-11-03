c26bb1e3d8678614f6a8e6dc0cb62e8c922ee29e
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-11-03 21:57:59 +0900

    Apply VSCode colors to `pre` tags

--- _config.ts
+++ _config.ts
@@ -164,6 +164,10 @@ site.use(tailwindcss({
         mono: ["monospace"],
       },
       extend: {
+        colors: {
+          "vsc-back": "#1f1f1f",
+          "vsc-front": "#d4d4d4",
+        },
         content: {
           "open-in-new": "url('/icon/open_in_new_16dp_2563EB.svg')",
           "open-in-new-gray": "url('/icon/open_in_new_16dp_6B7280.svg')",
