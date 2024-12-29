0322ffe1a16f1455ba1fc421583d567103bff53e
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-12-29 15:47:45 +0900

    Delete GitHub Pages deployments

--- terraform/cloudflare_pages.tf
+++ terraform/cloudflare_pages.tf
@@ -1,3 +1,9 @@
+resource "cloudflare_pages_domain" "pages_domain" {
+  account_id = var.cloudflare_account_id
+  domain = "akimo.dev"
+  project_name = "akimon658"
+}
+
 resource "cloudflare_pages_project" "deployment_config" {
   account_id = var.cloudflare_account_id
   name = "akimon658"
--- terraform/cloudflare_record.tf
+++ terraform/cloudflare_record.tf
@@ -1,72 +1,9 @@
-resource "cloudflare_record" "terraform_managed_resource_c2756f26180e4b09fbc2c9906d8a6eb0" {
-  content = "185.199.111.153"
+resource "cloudflare_record" "pages_domain" {
+  content = "akimon658.pages.dev"
   name    = "akimo.dev"
   proxied = true
   ttl     = 1
-  type    = "A"
-  zone_id = "39789e3a9e22c3a798baae245274330e"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_5451ab952c3b5940cdf6727e10a5726c" {
-  content = "185.199.110.153"
-  name    = "akimo.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-  zone_id = "39789e3a9e22c3a798baae245274330e"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_149c21aa87aa629ef671cbda6fea1a57" {
-  content = "185.199.109.153"
-  name    = "akimo.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-  zone_id = "39789e3a9e22c3a798baae245274330e"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_4e9d185ffba78b917537b524d024e265" {
-  content = "185.199.108.153"
-  name    = "akimo.dev"
-  proxied = true
-  ttl     = 1
-  type    = "A"
-  zone_id = "39789e3a9e22c3a798baae245274330e"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_d422735d56c60ec4b053094952b268c9" {
-  content = "2606:50c0:8003::153"
-  name    = "akimo.dev"
-  proxied = true
-  ttl     = 1
-  type    = "AAAA"
-  zone_id = "39789e3a9e22c3a798baae245274330e"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_80b0dc8f4454a59190930336aedf421b" {
-  content = "2606:50c0:8002::153"
-  name    = "akimo.dev"
-  proxied = true
-  ttl     = 1
-  type    = "AAAA"
-  zone_id = "39789e3a9e22c3a798baae245274330e"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_b856d9b8180eb2aa65a6d593720f6a00" {
-  content = "2606:50c0:8001::153"
-  name    = "akimo.dev"
-  proxied = true
-  ttl     = 1
-  type    = "AAAA"
-  zone_id = "39789e3a9e22c3a798baae245274330e"
-}
-
-resource "cloudflare_record" "terraform_managed_resource_7f095d8e58125d4b592728d03663e84b" {
-  content = "2606:50c0:8000::153"
-  name    = "akimo.dev"
-  proxied = true
-  ttl     = 1
-  type    = "AAAA"
+  type    = "CNAME"
   zone_id = "39789e3a9e22c3a798baae245274330e"
 }
 
